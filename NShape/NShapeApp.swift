522ca2b6d9aecd4ff5d7b77cc4ec9f4e0f17827c
AIshtaiwi <ish.ayham@gmail.com>
2024-12-29 16:46:09 -0500

    Initial Commit

---
+++ NShape/NShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NShapeApp.swift
+//  NShape
+//
+//  Created by Ayham Ishtaiwi on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct NShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NShapeTests/NShapeTests.swift
@@ -0,0 +1,17 @@
+//
+//  NShapeTests.swift
+//  NShapeTests
+//
+//  Created by Ayham Ishtaiwi on 12/29/24.
+//
+
+import Testing
+@testable import NShape
+
+struct NShapeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
